524aa48a0316adf2b91a86620ecb9c38fc6ea18a
viktor <viktorvanyatanya@gmail.com>
2022-08-24 14:42:42 +0200

    eslint

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-app.get('/', (_req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
 });
 
 app.listen(5500, () => {
+    // eslint-disable-next-line no-console
     console.log('Server has started on port 5500!!!');
 });
