a5261cd122b403b81d16fee1d5e4b06ae561d618
viktor <viktorvanyatanya@gmail.com>
2022-08-31 21:16:25 +0200

    mySql migration

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response } from 'express';
+// import { AppDataSource } from './data-source';
 
 const app = express();
+app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
 });
+// AppDataSource.initialize()
+//     .then(() => {
+//         // here you can start to work with your database
+//         console.log('Data Source has been initialized!');
+//     })
+//     .catch((error) => console.log(error));
 
-app.listen(5500, () => {
+app.listen(1337, () => {
     // eslint-disable-next-line no-console
     console.log('Server has started on port 5500!!!');
 });
--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,24 +1,18 @@
-import 'reflect-metadata';
-import { DataSource } from 'typeorm';
-
-export const AppDataSource = new DataSource({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'test',
-    password: 'test',
-    database: 'test',
-    synchronize: true,
-    logging: true,
-    entities: [],
-    subscribers: [],
-    migrations: [
-        'src/migration/**/*.ts',
-    ],
-});
-
-AppDataSource.initialize()
-    .then(() => {
-        // here you can start to work with your database
-    })
-    .catch((error) => console.log(error));
+// import 'reflect-metadata';
+// import { DataSource } from 'typeorm';
+//
+// export const AppDataSource = new DataSource({
+//     type: 'mysql',
+//     host: 'localhost',
+//     port: 3306,
+//     username: 'root',
+//     password: 'root',
+//     database: 'mysql',
+//     synchronize: true,
+//     logging: true,
+//     entities: [],
+//     subscribers: [],
+//     migrations: [
+//         'src/migration/**/*.ts',
+//     ],
+// });
