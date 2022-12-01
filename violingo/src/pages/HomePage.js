ea6bd39b6584f5d65b9ccdb62e7aa95572e998be
viktor <viktorvanyatanya@gmail.com>
2022-12-01 19:22:35 +0100

    add home page

--- violingo/src/pages/HomePage.js
+++ violingo/src/pages/HomePage.js
@@ -1,27 +1,49 @@
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
+ import earth from '../icons/earth.png';
+ import americaflag from '../icons/united-states.png';
+ import { MdKeyboardArrowDown, MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from 'react-icons/md';
 
 const HomePage = () =>  {
     return (
-        <OverlayTrigger
-        data-bs-custom-class="custom-tooltip"
-        placement={'buttom'}
-        overlay={
-          <Tooltip data-bs-custom-class="custom-tooltip">
-            <div className="div_example">
-                llslfkjdllskjfkljlsjlk
+      <div>
+        <header className="homePage_header">
+          <div className="homePage_div_sign">
+            <span className="homePage_sign_violingo">
+              violingo
+            </span>
+            <span className="homePage_sign_language">
+              мова сайту: украінська <MdKeyboardArrowDown size={'30px'}/>
+            </span>
+          </div> 
+        </header>
+        <div className="homePage_body">
+          <div className="homePage_div_center_body">
+            <div className="homePage_div_ringEarth1">
+              <div className="homePage_div_ringEarth2">
+                <img src={earth} alt="earth" className="homePage_image_earth"/>
+              </div>
             </div>
-            <div className="div_example">
-                lkljhhhhhhhhhhh
+            <div className="homePage_div_main_right">
+              <span className="homePage_span_sign_right">
+                  Безкоштовний, веселий та ефективний спосіб вивчення мови!
+              </span>
+              <span className="homePage_span_button1">
+                РОЗПОЧАТИ
+              </span>
+              <span className="homePage_span_button2">
+                УЖЕ МАЮ ОБЛІКОВИЙ ЗАПИС
+              </span>
             </div>
-            <div className="div_example">
-                lkshjjjjjjkjjjs
-            </div>
-          </Tooltip>
-        }
-      >
-        <div>Tooltip </div>
-      </OverlayTrigger>
+          </div>
+        </div>
+        <div className="homePage_div_choice_language">
+          <MdOutlineKeyboardArrowLeft size={'40px'} color={'#c5b9b9'}/>
+          <img src={americaflag} alt="america flag" className="homePage_image_choice_language"/>
+          <span className="homePage_span_sign_language">
+            АНГЛІЙСЬКА
+          </span>
+          <MdOutlineKeyboardArrowRight size={'40px'} color={'#c5b9b9'}/>
+        </div> 
+      </div>
     );
 };
 
