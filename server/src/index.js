78ebc28ca7867cf73b57f3c4d888008adc6a17f7
Viktor200719821974 <vik200719821974@gmail.com>
2023-11-06 20:38:33 +0100

    to added database

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Sequelize } = require('sequelize');
 
 const app = express();
 app.get('/', (req, res) => {
@@ -6,7 +7,29 @@ app.get('/', (req, res) => {
 });
 
 const PORT = 5500;
-app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server has started on port ${PORT}!!!`);
-});
+const sequelize = new Sequelize('postgres', 'postgres', 'root', {
+    host:'localhost',
+    dialect: 'postgres',
+    pool:{
+        max:5,
+        min:0,
+        idle:10000
+    },
+})
+const testDbConnection = async () => {
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+    }
+  };
+testDbConnection();
+const start = async() => {
+    await sequelize.sync();
+    app.listen(PORT, () => {
+        // eslint-disable-next-line no-console
+        console.log(`Server has started on port ${PORT}!!!`);
+    });
+}
+start();
