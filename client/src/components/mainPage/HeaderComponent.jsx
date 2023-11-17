07f918f768c9b2cd72291421eb4f7135168eb734
Viktor200719821974 <vik200719821974@gmail.com>
2023-11-17 19:29:28 +0100

    test commit

--- client/src/components/mainPage/HeaderComponent.jsx
+++ client/src/components/mainPage/HeaderComponent.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BiLogoZoom } from "react-icons/bi";
+
+import "../../style/HeaderComponent.css";
 
 const HeaderComponent = () => {
     return (
-        <header>
-            Head
+        <header className="main_header_headerComponent">
+            <div className="logo_div_headerComponent"> 
+                <BiLogoZoom size={"40px"}/>
+                <span className="logo_sign_span_headerComponent">Movies</span>
+            </div>
+            <div className="search_div_headerComponent">
+                <input type="text" placeholder="Search..." className="input_search_headerComponent"/>
+                <div className="icon_search_div_headerComponent">
+                    <AiOutlineSearch size={"30px"} color="#0f0f0f"/>
+                </div>
+               
+            </div>
         </header>
     );
 };
