dddd30fa6c4d3deb128f4f662a4e096ea662475d
Viktor200719821974 <vik200719821974@gmail.com>
2023-12-08 16:03:19 +0100

    added folder frontend

---
+++ frontend/precache-manifest.968fbec214fc1008003e4ddd33de0253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89046c54d86572930ed73827ad5bc2f5",
+    "url": "../frontend/index.html"
+  },
+  {
+    "revision": "339ec9c75d8fe3fbff35",
+    "url": "../frontend/static/css/main.c381d025.chunk.css"
+  },
+  {
+    "revision": "25e34e7e7d685d0d4bc0",
+    "url": "../frontend/static/js/2.6793c184.chunk.js"
+  },
+  {
+    "revision": "339ec9c75d8fe3fbff35",
+    "url": "../frontend/static/js/main.91b3c232.chunk.js"
+  },
+  {
+    "revision": "59494861bb997f83df07",
+    "url": "../frontend/static/js/runtime~main.c3d31710.js"
+  },
+  {
+    "revision": "bf426ed1ddce2c0a6a2c159cf511e47d",
+    "url": "../frontend/static/media/noImage.bf426ed1.png"
+  }
+]);
